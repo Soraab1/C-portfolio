47edb8fb87ea6e1aa789e539dff39a8e67200ad6
Soraab Bayat <132574017+Soraab1@users.noreply.github.com>
2023-08-31 21:45:39 +0200

    Add files via upload

---
+++ program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number: ");
+            string number = Console.ReadLine();
+
+            int numb = int.Parse(number);
+
+            if (numb % 3 == 0 || numb % 7 == 0 || numb == 1)
+            {
+                if (numb % 3 == 0)
+                {
+                    Console.WriteLine("The chosen number is divisible by 3");
+                }
+
+                if (numb % 7 == 0)
+                {
+                    Console.WriteLine("The chosen number is divisible by 7");
+                }
+
+                if (numb == 1)
+                {
+                    Console.WriteLine("The chosen number is equal to 1");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The chosen number doesn't meet any of the specified conditions.");
+            }
+
+            Console.Read();
+        }
+    }
+}
+
