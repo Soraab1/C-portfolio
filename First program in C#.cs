8da09c1c5759c46144949d1d231f2bf44ef0531f
Soraab Bayat <132574017+Soraab1@users.noreply.github.com>
2023-08-31 20:55:28 +0200

    Add files via upload

---
+++ First program in C#.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number: ");
+
+            string numb1 = Console.ReadLine();
+
+            int numb2= int.Parse(numb1);
+
+            if (numb2% 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
