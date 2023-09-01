5e73584544c7a57f760caa59b02e80b67ef1a03b
Soraab Bayat <132574017+Soraab1@users.noreply.github.com>
2023-09-01 16:17:21 +0200

    Add files via upload

---
+++ LogIn.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LogIn
+{
+
+    //basic program that mimicks a login, program only works with with letter username & password
+
+    public class exercise
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Choose a username: ");
+
+            string Username = Console.ReadLine();
+
+            Console.WriteLine("Use a password: ");
+
+            string Password = Console.ReadLine();
+
+
+            Console.WriteLine("Fill in your username: ");
+
+            string chosenUSR = Console.ReadLine();
+
+            if(chosenUSR == Username)
+            {
+
+                Console.WriteLine("the filled in username is correct ");
+            }
+            else
+            {
+
+                Console.WriteLine("the username you filled in is incorrect");
+            }
+            
+            Console.WriteLine("Fill in your password: ");
+
+            string chosenPWD = Console.ReadLine();
+
+            if (chosenPWD == Password)
+            {
+
+                Console.WriteLine("The filled in password is correct");
+
+            }
+            else
+            {
+
+                Console.WriteLine("The password you filled in is incorrect");
+            }
+
+
+            Console.Read();
+
+        }
+
+    } 
+
+}
+
+
+
